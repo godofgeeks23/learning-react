e776fded2bd158df241caad98bf9d7e7b52ae464
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-05 22:01:27 +0530

    Create index.js

---
+++ 0_cdn_setup/index.js
@@ -0,0 +1,11 @@
+ReactDOM.render(
+<ul>
+    <li>React is good</li>
+    <li>React is easy</li>
+</ul>,
+document.getElementById("root"))
+
+
+ReactDOM.render(
+<h1>This is React!</h1>,
+document.getElementById("root"))
