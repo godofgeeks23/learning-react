b79255772f5cf69d82ec6e0526f3d60b5a22c9b3
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-06-26 08:38:59 +0530

    Create ColorButton.jsx

---
+++ 04_color_changing_app/src/components/ColorButton.jsx
@@ -0,0 +1,12 @@
+function ColorButton({ color, onClick }) {
+  return (
+    <button
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      Change to {color}
+    </button>
+  );
+}
+
+export default ColorButton;
