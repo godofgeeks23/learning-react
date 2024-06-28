965c0d6fb1677c440665da645eea8aebbc5c7583
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-06-28 14:05:13 +0530

    Create Card.jsx

---
+++ 06_tailwind_cards/src/components/Card.jsx
@@ -0,0 +1,25 @@
+const Card = ({title, description, image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"}) => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-xl">
+      <figure>
+        <img
+          src={image}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{description}</p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
