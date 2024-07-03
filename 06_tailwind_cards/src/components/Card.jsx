1086ee3acc04e21098e1a3c600a72b84d5160afe
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-07-03 21:19:42 +0530

    Update Card.jsx

--- 06_tailwind_cards/src/components/Card.jsx
+++ 06_tailwind_cards/src/components/Card.jsx
@@ -1,11 +1,12 @@
-const Card = ({title, description, image="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"}) => {
+const Card = ({
+  title,
+  description,
+  image = "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg",
+}) => {
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure>
-        <img
-          src={image}
-          alt="Shoes"
-        />
+        <img src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">
