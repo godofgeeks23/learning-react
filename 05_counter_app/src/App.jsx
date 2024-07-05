c67c4a36eae9463182bde21715ae03067a568f8a
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-07-05 17:30:22 +0530

    Update App.jsx

--- 05_counter_app/src/App.jsx
+++ 05_counter_app/src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  function increaseCount(){
-    setCount(count + 1)
+  function increaseCount() {
+    setCount(count + 1);
   }
 
   return (
     <>
-    <h1>Currently value of count is {count}</h1>
-    <button onClick={() => increaseCount()}>Increment Counter</button>
+      <h1>Currently value of count is {count}</h1>
+      <button onClick={() => increaseCount()}>Increment Counter</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
