f8c3e84bdaaaf5e9a79a9dad1396a9a3c285d27a
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-07-12 22:36:00 +0530

    Update App.jsx

--- 09_upcoming_proj/src/App.jsx
+++ 09_upcoming_proj/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // count variable
+  // count variable and setCount function are used to update the count value
   const [count, setCount] = useState(0);
 
   return (
