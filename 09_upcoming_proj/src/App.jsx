5b2ee6b2af687cf15a1387e7552b3c981b594a7d
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-07-16 21:52:05 +0530

    Update App.jsx

--- 09_upcoming_proj/src/App.jsx
+++ 09_upcoming_proj/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite and React</h1>
+      <h1>Vite+React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite+React logos to learn more
       </p>
     </>
   );
